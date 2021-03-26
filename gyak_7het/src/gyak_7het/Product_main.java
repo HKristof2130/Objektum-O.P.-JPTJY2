bc95e45f4e31e0fa99995786487559a06bdbfd9b
HKristof2130 <kmhegyesi1@gmail.com>
2021-03-26 04:52:51 +0100

    Create Product_main.java

---
+++ gyak_7het/src/gyak_7het/Product_main.java
@@ -0,0 +1,12 @@
+package gyak_7het;
+
+public class Product_main {
+
+	public static void main(String[] args) {
+
+		Product prod = new Product("Laptop",200000,27);
+		Bread bread= new Bread("Kenyerke",150,27,5);
+		
+	}
+
+}
