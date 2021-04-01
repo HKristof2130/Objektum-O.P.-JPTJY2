d2ef0263b7d2599170966d5aafd3c4581a9b0441
HKristof2130 <kmhegyesi1@gmail.com>
2021-04-01 14:15:27 +0200

    Create Words.java

---
+++ Gyakorlás/src/random_sentence_generator/Words.java
@@ -0,0 +1,14 @@
+package random_sentence_generator;
+
+public class Words {
+	
+	String[] basicWord1= {"Egyszer","Sosem","Mindig"};
+	String[] basicWord2= {"volt","lesz","van"};
+	String[] basicWord3= {"vége","kezdete","közepe"};
+
+	String[] newWord1;
+	String[] newWord2;
+	String[] newWord3;
+
+
+}
